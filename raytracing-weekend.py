3267a85603c494840c661f08d7b7bcce8f659141
racoonraduggy12 <danielbeeman2009@gmail.com>
2025-12-19 14:39:25 -0600

    Add files via upload

---
+++ raytracing-weekend.py
@@ -0,0 +1,45 @@
+import math
+import logging
+import numpy as np
+
+logger = logging.getLogger(__name__)
+
+logging.basicConfig(filename='raytracingweekend.log', level=logging.INFO)
+
+class vec3:
+    def __init__(self, e0=0, e1=0, e2=0):
+        e = [e0, e1, e2]
+        e = np.array(e)
+        self.e = e
+    def x(self):
+        return self.e[0]
+    def y(self):
+        return self.e[1]
+    def z(self):
+        return self.e[2]
+    def __sub__(self, other):
+        return vec3(self.e[0] - other.e[0], self.e[1] - other.e[1], self.e[2] - other.e[2])
+
+test = vec3(1,2,3)
+test = vec3() - test
+logger.info(str(test.x())+str(test.y())+str(test.z()))
+image_height = 256
+image_width = 256
+print("P3\n"+str(image_width)+' '+str(image_height)+"\n255\n")
+
+j = 0
+while j < image_height:
+    i = 0
+    logger.info("\rScanlines remaining: "+str(image_height-j))
+    while i < image_width:
+        r = i/(image_width-1)
+        g = j/(image_height-1)
+        b = 0.00
+
+        ir = round(254.99 * r)
+        ig = round(254.99 * g)
+        ib = round(254.99 * b)
+
+        print(str(ir)+' '+str(ig)+' '+str(ib)+'\n')
+        i += 1
+    j += 1
