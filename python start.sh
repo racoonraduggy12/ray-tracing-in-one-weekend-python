1bff08e13b375f3a5ea5e6c45c6428543e2aad04
racoonraduggy12 <danielbeeman2009@gmail.com>
2025-12-20 22:48:32 -0600

    fixed launch script

--- python start.sh
+++ python start.sh
@@ -1,6 +1,7 @@
-source /venv/bin/activate
+cd "$( dirname "${BASH_SOURCE[0]}" )"
+source venv/bin/activate
 which python
 echo python venv started, install modules to your hearts content
 echo path is:
 pwd
-python /home/daniel/Documents/coding/python/raytracing-weekend.py > /home/daniel/Documents/coding/python/outputimage.ppm
+python raytracing-weekend.py > outputimage.ppm
